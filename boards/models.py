0039813d24a4510447112b8e3203851ffb82e95b
unknown <vitobassily@gmail.com>
2022-11-22 21:08:53 +0200

    created models & migrate data

--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+ 
+class Boards (models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=150)
+
+class Topic (models.Model):
+    subject = models.CharField(max_length=250)
+    board = models.ForeignKey(Boards, related_name='topic', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User ,related_name='topics',on_delete=models.CASCADE)
+    created_at =models.DateTimeField(auto_now_add=True)
+
+class Post(models.Model):
+    message = models.TextField(max_length=4000)
+    topic = models.ForeignKey(Topic, related_name='posts',on_delete=models.CASCADE)
+    created_by= models.ForeignKey(User,related_name='posts', on_delete=models.CASCADE)
+    created_at= models.DateTimeField(auto_now_add=True)
+
